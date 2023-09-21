db99e1afa22d23e84142b96720f02d013ed64690
Lewis Shum <lewisshum.work@gmail.com>
2023-09-21 18:24:24 +0000

    Remove unnecessary console printed output and uncomment code for insertion to db

--- practice/linux_quote_app/quote_app.sh
+++ practice/linux_quote_app/quote_app.sh
@@ -15,22 +15,14 @@ output=$(curl --request GET \
 	--header 'X-RapidAPI-Host: alpha-vantage.p.rapidapi.com' \
 	--header "X-RapidAPI-Key: ${key}" | jq '."Global Quote"')
 
-echo "$output"
 open=$(echo "$output" | head -3 | tail -1 | awk '{print $3}' | sed 's/"//g' | sed 's/,//g')
 high=$(echo "$output" | head -4 | tail -1 | awk '{print $3}' | sed 's/"//g' | sed 's/,//g')
 low=$(echo "$output" | head -5 | tail -1 | awk '{print $3}' | sed 's/"//g' | sed 's/,//g')
 price=$(echo "$output" | head -6 | tail -1 | awk '{print $3}' | sed 's/"//g' | sed 's/,//g')
 volume=$(echo "$output" | head -7 | tail -1 | awk '{print $3}' | sed 's/"//g' | sed 's/,//g')
 
-echo "symbol: ${symbol}"
-echo "open: ${open}"
-echo "high: ${high}"
-echo "low: ${low}"
-echo "price: ${price}"
-echo "volume: ${volume}"
+insert_stmt="INSERT INTO quotes (symbol, open, high, low, price, volume)
+            VALUES('$symbol', $open, $high, $low, $price, $volume);"
 
-#insert_stmt="INSERT INTO quotes (symbol, open, high, low, price, volume)
-#            VALUES('$symbol', '$open', '$high', '$low', '$price', '$volume');"
-#
-#export PGPASSWORD=$password
-#psql -h "$host" -p "$port" -d "$database" -U "$username" -c "$insert_stmt"
+export PGPASSWORD=$password
+psql -h "$host" -p "$port" -d "$database" -U "$username" -c "$insert_stmt"
